1407d8635a11f53ae45968e82b450f240e09fc74
usuario <usuario@usuario-PC>
2018-11-09 20:58:56 -0300

    Clase Sistema Avanzada_v2

--- src/mascota/Sistema.java
+++ src/mascota/Sistema.java
@@ -13,13 +13,14 @@ import java.util.Iterator;
  * @author Emiliano
  */
 public class Sistema {
+
     //Atributos
-    private ArrayList<Mascota> listadoMascota; 
-    private ArrayList<Familiar> listadoFamiliar; 
-    private ArrayList<Actividad> listadoActividades; 
-    private ArrayList<Alimento> listadoAlimento; 
+    private ArrayList<Mascota> listadoMascota;
+    private ArrayList<Familiar> listadoFamiliar;
+    private ArrayList<Actividad> listadoActividades;
+    private ArrayList<Alimento> listadoAlimento;
     private ArrayList<Servicio> listadoServicio;
-    
+
     //Constructor
     public Sistema() {
         listadoMascota = new ArrayList<Mascota>();
@@ -28,61 +29,61 @@ public class Sistema {
         listadoAlimento = new ArrayList<Alimento>();
         listadoServicio = new ArrayList<Servicio>();
     }
-    
+
     //Métodos de acceso
-    public ArrayList<Mascota> getListaMascotas(){
+    public ArrayList<Mascota> getListaMascotas() {
         return listadoMascota;
     }
-    
-    public ArrayList<Familiar> getListaFamiliares(){
+
+    public ArrayList<Familiar> getListaFamiliares() {
         return listadoFamiliar;
     }
-    
-    public ArrayList<Actividad> getListaActividades(){
+
+    public ArrayList<Actividad> getListaActividades() {
         return listadoActividades;
     }
-    
-    public ArrayList<Alimento> getListaAlimentos(){
+
+    public ArrayList<Alimento> getListaAlimentos() {
         return listadoAlimento;
     }
-    
-    public ArrayList<Servicio> getListaServicios(){
+
+    public ArrayList<Servicio> getListaServicios() {
         return listadoServicio;
     }
-    
+
     //Agregar listado al sistema    
     public void agregoMascotaALista(Mascota mascota) {
         this.getListaMascotas().add(mascota);
     }
-    
+
     public void agregoFamiliarALista(Familiar familiar) {
         this.getListaFamiliares().add(familiar);
     }
-    
+
     public void agregoActividadALista(Actividad actividad) {
         this.getListaActividades().add(actividad);
     }
-    
+
     public void agregoAlimentoALista(Alimento alimento) {
         this.getListaAlimentos().add(alimento);
     }
-    
+
     public void agregoServicioALista(Servicio servicio) {
         this.getListaServicios().add(servicio);
     }
-    
+
     //Eliminar elemento del listado
     public void eliminarMascotaDeLista(Mascota mascota) {
         this.getListaMascotas().remove(mascota);
     }
-    
+
     public void eliminarFamiliarDeLista(Familiar familiar) {
         this.getListaFamiliares().remove(familiar);
     }
-    
+
     //Método que valida que no exista mascota con el mismo nombre en el listado
-    public boolean validoNombreMascota(ArrayList<Mascota> lista, 
-    String nombre) {
+    public boolean validoNombreMascota(ArrayList<Mascota> lista,
+            String nombre) {
         boolean esValido = true;
         Iterator<Mascota> it = lista.iterator();
         while (it.hasNext()) {
@@ -93,10 +94,10 @@ public class Sistema {
         }
         return esValido;
     }
-    
+
     //Método que valida que no exista familiar con el mismo nombre en el listado
-    public boolean validoNombreFamiliar(ArrayList<Familiar> lista, 
-    String nombre) {
+    public boolean validoNombreFamiliar(ArrayList<Familiar> lista,
+            String nombre) {
         boolean esValido = true;
         Iterator<Familiar> it = lista.iterator();
         while (it.hasNext()) {
@@ -106,5 +107,10 @@ public class Sistema {
             }
         }
         return esValido;
+    }
+
+    //Métodos de modificación de elementos de los listados
+    public boolean modificarMascota(ArrayList<Mascota> listado, Mascota mas) {
+    return true;
     }
 }
